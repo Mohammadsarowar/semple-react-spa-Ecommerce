62dbdae06bad667c94554b19629bdebfe7713d2e
Md Sarowar <mdsarowarhang@gmail.com>
2023-03-28 12:37:50 +0300

    last commit

--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import './Products.css'
+import './Products.css';
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faShoppingCart} />
+
+
+
+
 const Products = (props) => {
+  // const HandleAddTOCard = (product) =>{
+  //   console.log(product)
+  //    }
     const {name,price,id,ratings,img,seller} = props.pro
+    const HandleAddTOCard = props.HandleAddTOCard
     return (
         <div className='products'> 
           <img src={img}></img>
@@ -11,7 +24,7 @@ const Products = (props) => {
             <p>Manufacturer: {seller}</p>
             <p>Rating: {ratings} start</p>
           </div>
-          <button className='btn'>Add to card</button> 
+          <button onClick={()=>HandleAddTOCard(props.pro)} className='btn'>Add to card {element}</button> 
         </div>
     );
 };
--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -4,11 +4,16 @@ import './Shop.css'
 
 const Shop = () => {
     const [products,setProducts] = useState([])
+    const [save,setSave] = useState([])
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data))
-    },[])
+    },[]);
+    const HandleAddTOCard = (product) =>{
+      const newCard = [...save,product];
+      setSave(newCard);
+       }
     return (
     <div className='shop-container'>
         <div className='products-container'>
@@ -16,11 +21,13 @@ const Shop = () => {
           products.map(product=><Products
           key={product.id}
           pro={product}
+          HandleAddTOCard={HandleAddTOCard}
           ></Products>)
          }
         </div>
         <div className='card-container'>
         <h3>Order Summary</h3>
+        <p>Selected Items: {save.length}</p>
         </div>
     </div>
     );
